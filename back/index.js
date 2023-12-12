6629d1b59d90d6eb683246f9d86c08a77636ce58
JPConti <juanpcsilva@gmail.com>
2023-12-11 23:33:26 -0300

    update and delete

--- back/index.js
+++ back/index.js
@@ -1,123 +1,199 @@
 require("dotenv").config();
 
-const express = require('express');
+const express = require("express");
 const app = express();
-const fs = require('fs');
-const cors = require('cors');
-const multer = require('multer');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('./model/User');
-const Livro = require('./model/Livro');
+const fs = require("fs");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("./model/User");
+const Livro = require("./model/Livro");
 const upload = multer();
 app.use(express.json());
 app.use(cors());
 
-
-app.listen(3000, () =>{
-
-    console.log("Servidor ouvindo na porta 3000!!!");
+app.listen(3000, () => {
+  console.log("Servidor ouvindo na porta 3000!!!");
 });
 
-app.post('/login', async (req,res) => {
+app.post("/login", async (req, res) => {
+  const { usuario, senha } = req.body;
+  const jsonPath = path.join(__dirname, ".", "db", "banco-dados-usuario.json");
+  const usuariosCadastrados = JSON.parse(
+    fs.readFileSync(jsonPath, { encoding: "utf8", flag: "r" })
+  );
 
-    const {usuario, senha} = req.body; 
-    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
-    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+  for (let user of usuariosCadastrados) {
+    if (user.usuario === usuario) {
+      const senhaValidada = await bcrypt.compare(senha, user.senha);
+      if (senhaValidada) {
+        const token = jwt.sign(
+          { userId: user.id, usuario: user.usuario },
+          process.env.TOKEN
+        );
 
-    for (let user of usuariosCadastrados){
-        if(user.usuario === usuario){
-            const senhaValidada = await bcrypt.compare(senha, user.senha);
-            if(senhaValidada){ 
-                
-                const token = jwt.sign({ userId: user.id, usuario: user.usuario }, process.env.TOKEN);
-
-                return res.json({ "token" : token,  "userId" : user.id});
-            }
-            
-            else
-                return res.status(422).send(`Usuario ou senhas incorretas.`);
-        }   
+        return res.json({ token: token, userId: user.id });
+      } else return res.status(422).send(`Usuario ou senhas incorretas.`);
     }
-
+  }
 });
 
-app.post('/criar', async (req,res) => {
-    const {usuario, email, senha, confirmasenha} = req.body; 
-    const jsonPath = path.join(__dirname, '.', 'db', 'banco-dados-usuario.json');
-    const usuariosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+app.post("/criar", async (req, res) => {
+  const { usuario, email, senha, confirmasenha } = req.body;
+  const jsonPath = path.join(__dirname, ".", "db", "banco-dados-usuario.json");
+  const usuariosCadastrados = JSON.parse(
+    fs.readFileSync(jsonPath, { encoding: "utf8", flag: "r" })
+  );
 
-    const usuarioExistente = usuariosCadastrados.find(user => user.usuario === usuario);
-    const emailExistente = usuariosCadastrados.find(user => user.email === email);
+  const usuarioExistente = usuariosCadastrados.find(
+    (user) => user.usuario === usuario
+  );
+  const emailExistente = usuariosCadastrados.find(
+    (user) => user.email === email
+  );
 
-    if (!usuario || !senha || !email || !confirmasenha) {
-        return res.status(409).send(`Você deve preencher todos os campos..`);
-    }
+  if (!usuario || !senha || !email || !confirmasenha) {
+    return res.status(409).send(`Você deve preencher todos os campos..`);
+  }
 
-    if (usuarioExistente) {
-        return res.status(409).send(`Usuário ${usuario} já foi cadastrado.`);
-    }
+  if (usuarioExistente) {
+    return res.status(409).send(`Usuário ${usuario} já foi cadastrado.`);
+  }
 
-    if (emailExistente) {
-        return res.status(409).send(`E-mail ${email} já foi cadastrado.`);
-    }
+  if (emailExistente) {
+    return res.status(409).send(`E-mail ${email} já foi cadastrado.`);
+  }
 
-    if (senha.length < 4) {
-        return res.status(409).send(`A senha deve conter pelo menos 4 dígitos, tente novamente.`);
-    }
+  if (senha.length < 4) {
+    return res
+      .status(409)
+      .send(`A senha deve conter pelo menos 4 dígitos, tente novamente.`);
+  }
 
-    if(confirmasenha !== senha){
-        return res.status(409).send(`As senhas não são iguais, tente novamente..`);
-    }
+  if (confirmasenha !== senha) {
+    return res.status(409).send(`As senhas não são iguais, tente novamente..`);
+  }
 
-    const id = usuariosCadastrados.length + 1;
+  const id = usuariosCadastrados.length + 1;
 
-    const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10);
 
-    const senhaCripto = await bcrypt.hash(senha,salt);
+  const senhaCripto = await bcrypt.hash(senha, salt);
 
-    const user = new User(id, usuario, email, senhaCripto);
+  const user = new User(id, usuario, email, senhaCripto);
 
-    usuariosCadastrados.push(user);
-    fs.writeFileSync(jsonPath,JSON.stringify(usuariosCadastrados,null,2));
-    res.send(`Tudo certo usuario criado com sucesso.`);
+  usuariosCadastrados.push(user);
+  fs.writeFileSync(jsonPath, JSON.stringify(usuariosCadastrados, null, 2));
+  res.send(`Tudo certo usuario criado com sucesso.`);
 });
 
-app.post('/postar', upload.none(), async (req,res) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+app.post("/postar", upload.none(), async (req, res) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if (token == null) return res.sendStatus(401); 
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.TOKEN, (err, user) => {
+    if (err) return res.sendStatus(403);
+
+    req.user = user;
+
+    const { titulo, autor, editora, pagnum, img } = req.body;
+    const userId = req.user.userId;
+    const jsonPath = path.join(__dirname, ".", "db", "livros.json");
+    const livrosCadastrados = JSON.parse(
+      fs.readFileSync(jsonPath, { encoding: "utf8", flag: "r" })
+    );
+
+    const livroExistente = livrosCadastrados.find(
+      (livro) => livro.titulo === titulo && livro.userId === userId
+    );
+    if (!titulo || !autor || !editora || !pagnum || !img || !userId) {
+      return res.status(409).send(`Você deve preencher todos os campos..`);
+    }
+
+    if (livroExistente) {
+      return res.status(409).send(`Livro ${titulo} já foi cadastrado.`);
+    }
+
+    const id = livrosCadastrados.length + 1;
+    const livro = new Livro(id, titulo, autor, editora, pagnum, img, userId);
+
+    livrosCadastrados.push(livro);
+    fs.writeFileSync(jsonPath, JSON.stringify(livrosCadastrados, null, 2));
+    res.send(`Tudo certo, livro cadastrado com sucesso!!`);
+  });
+
+  app.put("/atualizar/:livroId", upload.none(), async (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) return res.sendStatus(401);
 
     jwt.verify(token, process.env.TOKEN, (err, user) => {
-        if (err) return res.sendStatus(403); 
+      if (err) return res.sendStatus(403);
 
-        req.user = user;
+      req.user = user;
 
+      const livroId = parseInt(req.params.livroId);
+      const { titulo, autor, editora, pagnum, img } = req.body;
+      const userId = req.user.userId;
+      const jsonPath = path.join(__dirname, ".", "db", "livros.json");
+      const livrosCadastrados = JSON.parse(
+        fs.readFileSync(jsonPath, { encoding: "utf8", flag: "r" })
+      );
 
-        const {titulo, autor, editora, pagnum, img} = req.body;
-        const userId = req.user.userId; 
-        const jsonPath = path.join(__dirname, '.', 'db', 'livros.json');
-        const livrosCadastrados = JSON.parse(fs.readFileSync(jsonPath, { encoding: 'utf8', flag: 'r' }));
+      const livroIndex = livrosCadastrados.findIndex(
+        (livro) => livro.id === livroId && livro.userId === userId
+      );
 
-        const livroExistente = livrosCadastrados.find(livro => livro.titulo === titulo && livro.userId === userId); 
-        if (!titulo || !autor || !editora || !pagnum || !img || !userId ) {
+      if (livroIndex === -1) {
+        return res.status(404).send(`Livro não encontrado.`);
+      }
 
-            return res.status(409).send(`Você deve preencher todos os campos..`);
-        }
+      livrosCadastrados[livroIndex].titulo = titulo;
+      livrosCadastrados[livroIndex].autor = autor;
+      livrosCadastrados[livroIndex].editora = editora;
+      livrosCadastrados[livroIndex].pagnum = pagnum;
+      livrosCadastrados[livroIndex].img = img;
 
-        if (livroExistente) {
-
-            return res.status(409).send(`Livro ${titulo} já foi cadastrado.`);
-        }
-
-        const id = livrosCadastrados.length + 1;
-        const livro = new Livro(id, titulo, autor, editora, pagnum, img, userId); 
-
-        livrosCadastrados.push(livro);
-        fs.writeFileSync(jsonPath,JSON.stringify(livrosCadastrados,null,2));
-        res.send(`Tudo certo, livro cadastrado com sucesso!!`);
+      fs.writeFileSync(jsonPath, JSON.stringify(livrosCadastrados, null, 2));
+      res.send(`Livro atualizado com sucesso!!`);
     });
-});
+  });
 
+  app.delete("/excluir/:livroId", async (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.TOKEN, (err, user) => {
+      if (err) return res.sendStatus(403);
+
+      req.user = user;
+
+      const livroId = parseInt(req.params.livroId);
+      const userId = req.user.userId;
+      const jsonPath = path.join(__dirname, ".", "db", "livros.json");
+      const livrosCadastrados = JSON.parse(
+        fs.readFileSync(jsonPath, { encoding: "utf8", flag: "r" })
+      );
+
+      const livroIndex = livrosCadastrados.findIndex(
+        (livro) => livro.id === livroId && livro.userId === userId
+      );
+
+      if (livroIndex === -1) {
+        return res.status(404).send(`Livro não encontrado.`);
+      }
+
+      livrosCadastrados.splice(livroIndex, 1);
+
+      fs.writeFileSync(jsonPath, JSON.stringify(livrosCadastrados, null, 2));
+      res.send(`Livro excluído com sucesso!!`);
+    });
+  });
+});
